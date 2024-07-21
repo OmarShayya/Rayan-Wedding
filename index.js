eb8dc8bffc191d5c7b6be83cf89987ba9ecac87d
OShayya <oshayya@acksession.com>
2024-07-21 22:24:28 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+let currentIndex = 0;
+
+function slideImages() {
+    const background = document.querySelector('.background');
+    const images = background.querySelectorAll('img');
+    currentIndex = (currentIndex + 1) % images.length;
+    background.style.transform = `translateX(-${currentIndex * 100}vw)`;
+}
+
+setInterval(slideImages, 3000);
